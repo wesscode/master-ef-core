3aac8a5d76efc09d2a020e379d6157354ce005f7
Wesley Alves <wesley.info07@gmail.com>
2023-02-18 12:01:17 -0300

    Gap EnsureCreated com multiplos contextos

---
+++ Data/ApplicationContextCity.cs
@@ -0,0 +1,25 @@
+﻿using MasterEFCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterEFCore.Data
+{
+    public class ApplicationContextCity : DbContext
+    {
+        public DbSet<City> cities { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            const string strConnection = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=MasterEFCore; Integrated Security=True";
+            optionsBuilder
+                .UseSqlServer(strConnection)
+                .EnableSensitiveDataLogging()
+                .LogTo(Console.WriteLine, LogLevel.Information);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using MasterEFCore.Data;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 
 Console.WriteLine("Hello, World!");
 //EnsureCreatedAndDelete();
+GapDoEnsureCreated();
 
 static void EnsureCreatedAndDelete()
 {
@@ -10,4 +13,18 @@ static void EnsureCreatedAndDelete()
     using var db = new ApplicationContext();
     db.Database.EnsureCreated();
     //db.Database.EnsureDeleted();
+}
+
+static void GapDoEnsureCreated()
+{
+    //gap ao ter multiplos contextos, ele so entende e executa o primeiro.
+    using var db1 = new ApplicationContext();
+    using var db2 = new ApplicationContextCity();
+
+    db1.Database.EnsureCreated();
+    db2.Database.EnsureCreated();
+
+    //solução para forçar execução do segundo contexto.
+    var databaseCreator = db2.GetService<IRelationalDatabaseCreator>();
+    databaseCreator.CreateTables();
 }
