7bdbdb8900a8bb273d15643f30b409e68cf6aec9
Wesley Alves <wesley.info07@gmail.com>
2023-05-01 20:10:28 -0300

    Gerador SQL customizado

---
+++ EFCore.QuerySqlGenerator/MySqlServerQuerySqlGenerator.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Linq.Expressions;
+
+namespace EFCore.QuerySqlGenerator
+{
+    public class MySqlServerQuerySqlGenerator : SqlServerQuerySqlGenerator
+    {
+        //Injetado no construtor as infraestrutura que ele precisa, para gerar os comandos sql
+        public MySqlServerQuerySqlGenerator(QuerySqlGeneratorDependencies dependencies, IRelationalTypeMappingSource typeMappingSource) : base(dependencies, typeMappingSource)
+        {
+        }
+
+        //quando for "visitar" uma tabela
+        protected override Expression VisitTable(TableExpression tableExpression)
+        {
+            var table = base.VisitTable(tableExpression);
+            Sql.Append(" WITH (NOLOCK)");
+
+            return table;
+        }
+    }
+}
