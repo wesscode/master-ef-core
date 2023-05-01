f5ade6f4dc046bbe20228065c15ed53f5da20518
Wesley Alves <wesley.info07@gmail.com>
2023-05-01 19:54:11 -0300

    Preparando ambiente.

---
+++ EFCore.QuerySqlGenerator/Data/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using EFCore.QuerySqlGenerator.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.QuerySqlGenerator.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Departamento> Departamentos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer("Data Source=(localdb)\\mssqllocaldb; Database=SobrescrevendoComportamento; Integrated Security=true")
+                .LogTo(Console.WriteLine)
+                .EnableSensitiveDataLogging();
+        }
+    }
+}
---
+++ EFCore.QuerySqlGenerator/Program.cs
@@ -0,0 +1,7 @@
+﻿static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("oi");
+    }
+}
